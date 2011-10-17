2652e8a0dbae4056614258cfed31fbe215e2fbe6
Alex Nowak <alex.nowak@gmx.de>
2011-10-17 15:30:58 -0400

    Session Controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
   def has_password?(submitted_password)
     # Compare encrypted_password with the encrypted version of
     # submitted_password.
-	encrypted_password == encrypt(submitted_password)
+	  encrypted_password == encrypt(submitted_password)
   end
 
   # @param email [String]
--- spec/factory.rb
+++ spec/factory.rb
@@ -1,7 +1,9 @@
 # By using the symbol ':user', we get Factory Girl to simulate the User model.
-Factory.define :user do |user|
-  user.name 					"Michael Hartl"
-  user.email					"mhartl@example.com"
-  user.password					"foobar"
-  user.password_confirmation	"foobar"
+FactoryGirl.define do
+  factory :user do
+    name 					         "Michael Hartl"
+    email					        "mhartl@example.com"
+    password					      "foobar"
+    password_confirmation	"foobar"
+  end
 end
