e0c744a2972919a7d26d761652a62cfe353d17cb
Alex Nowak <alex.nowak@gmx.de>
2012-01-02 14:07:46 -0500

    signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,14 +4,25 @@ class SessionsController < ApplicationController
   end
 
   def create
+    
+    puts "params[:session][:email]=#{params[:session][:email]}"
+    puts "params[:session][:password]=#{params[:session][:password]}"
+    
     user = User.authenticate(params[:session][:email],
                              params[:session][:password])
+
+    puts "user=#{user}"
+
     if user.nil?
+      puts "user not found. Flash an error..."
       flash.now[:error] = "Invalid username/password combination."
       @title = "Sign in"
       render 'new'
     else
       # Sign the user in and redirect to the user's show page.
+      puts "User is authenticated successfully!"
+      sign_in user
+      redirect_to user
     end
 
   end
